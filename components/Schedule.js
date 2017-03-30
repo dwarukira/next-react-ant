e38297a052c1a81dbeb73bc6f55975b4b684fb5f
Admin <admin@CC-VL.local>
2017-03-30 11:56:29 +0800

    - add validation to timepicker

--- components/Schedule.js
+++ components/Schedule.js
@@ -1,10 +1,10 @@
 import { PureComponent } from 'react';
-import { Select, Modal, Button, Row, Col, TimePicker, Checkbox } from 'antd';
-import moment from 'moment';
+import { Form, Select, Modal, Button, Row, Col, Checkbox } from 'antd';
+
+import TimePicker from './TimePicker';
 
 const Option = Select.Option;
 const CheckboxGroup = Checkbox.Group;
-const format = 'HH:mm';
 
 const plainOptions = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
 const defaultCheckedList = ['MON', 'FRI'];
@@ -25,7 +25,7 @@ const styleCheckAll = {
   marginLeft: '10px'
 };
 
-export default class Schedule extends PureComponent {
+class Schedule extends PureComponent {
   state = {
     checkedList: defaultCheckedList,
     indeterminate: true,
@@ -47,137 +47,147 @@ export default class Schedule extends PureComponent {
       return (i + 1).toString();
     });
     const isFrequencyMinutes = frequencyType === 'minutes';
+
+    const { getFieldDecorator } = this.props.form;
+
     return (
       <div>
         <Modal
-          title="How frequent should this feed be run?"
+          title="How frequent should this feed run?"
           visible={visible}
           onOk={this.handleOk}
           confirmLoading={this.state.confirmLoading}
           onCancel={this.handleCancel}
         >
-          <Row gutter={RowProps.Gutter} style={styleRow}>
-            <Col span={24}>
-              <div
-                style={{
-                  paddingBottom: '8px',
-                  marginBottom: '8px'
-                }}
-                className="ant-border-bottom"
-              >
-                Days{`   `}
-                <Checkbox
-                  style={styleCheckAll}
-                  indeterminate={this.state.indeterminate}
-                  onChange={this.onCheckAllDaysChange}
-                  checked={this.state.checkAll}
+          <Form>
+
+            <Row gutter={RowProps.Gutter} style={styleRow}>
+              <Col span={24}>
+                <div
+                  style={{
+                    paddingBottom: '8px',
+                    marginBottom: '8px'
+                  }}
+                  className="ant-border-bottom"
                 >
-                  Everyday
-                </Checkbox>
-              </div>
+                  Days{`   `}
+                  <Checkbox
+                    style={styleCheckAll}
+                    indeterminate={this.state.indeterminate}
+                    onChange={this.onCheckAllDaysChange}
+                    checked={this.state.checkAll}
+                  >
+                    Everyday
+                  </Checkbox>
+                </div>
 
-              <CheckboxGroup
-                options={plainOptions}
-                value={this.state.checkedList}
-                onChange={this.onChangeDays}
-              />
-            </Col>
-          </Row>
+                <CheckboxGroup
+                  options={plainOptions}
+                  value={this.state.checkedList}
+                  onChange={this.onChangeDays}
+                />
+              </Col>
+            </Row>
 
-          <p>Frequency</p>
-          <Row gutter={RowProps.Gutter} style={styleRow}>
-            <Col span={12}>
-              <Select
-                style={styleFormControl}
-                value={frequencyValue}
-                onChange={frequencyValue => {
-                  this.setState({
-                    frequencyValue
-                  });
-                }}
-              >
-                {isFrequencyMinutes
-                  ? minutes.map(minute => (
-                      <Option key={minute} value={minute}>{minute}</Option>
-                    ))
-                  : hours.map(hour => (
-                      <Option key={hour} value={hour}>{hour}</Option>
-                    ))}
+            <p style={{ marginBottom: '8px' }}>Frequency</p>
+            <Row gutter={RowProps.Gutter} style={styleRow}>
+              <Col span={12}>
+                <Select
+                  style={styleFormControl}
+                  value={frequencyValue}
+                  onChange={frequencyValue => {
+                    this.setState({
+                      frequencyValue
+                    });
+                  }}
+                >
+                  {isFrequencyMinutes
+                    ? minutes.map(minute => (
+                        <Option key={minute} value={minute}>{minute}</Option>
+                      ))
+                    : hours.map(hour => (
+                        <Option key={hour} value={hour}>{hour}</Option>
+                      ))}
 
-              </Select>
-            </Col>
-            <Col span={12}>
-              <Select
-                // ref={c => this.frequencyType = c}
-                style={styleFormControl}
-                value={frequencyType}
-                onChange={frequencyType => {
-                  this.setState({
-                    frequencyType,
-                    frequencyValue: '1'
-                  });
-                }}
-              >
-                <Option value="minutes">minute(s)</Option>
-                <Option value="hours">hours(s)</Option>
-              </Select>
-            </Col>
-          </Row>
-          <Row gutter={RowProps.Gutter} style={styleRow}>
-            <Col span={12}>
-              <p>
-
-                Start Time
-              </p>
-              <TimePicker
-                value={moment(startTime, format)}
-                format={format}
-                onChange={this.onChangeStartTime}
-              />
-            </Col>
-            <Col span={12}>
-              <p>
-                End Time
-              </p>
-              <TimePicker
-                value={moment(endTime, format)}
-                format={format}
-                onChange={this.onChangeEndTime}
-              />
-            </Col>
-          </Row>
+                </Select>
+              </Col>
+              <Col span={12}>
+                <Select
+                  style={styleFormControl}
+                  value={frequencyType}
+                  onChange={frequencyType => {
+                    this.setState({
+                      frequencyType,
+                      frequencyValue: '1'
+                    });
+                  }}
+                >
+                  <Option value="minutes">minute(s)</Option>
+                  <Option value="hours">hours(s)</Option>
+                </Select>
+              </Col>
+            </Row>
+            <Row gutter={RowProps.Gutter} style={styleRow}>
+              <Col span={12}>
+                <TimePicker
+                  label="Start Time"
+                  field="startTime"
+                  getFieldDecorator={getFieldDecorator}
+                  onChange={this.onChangeStartTime}
+                />
+              </Col>
+              <Col span={12}>
+                <TimePicker
+                  label="End Time"
+                  field="endTime"
+                  getFieldDecorator={getFieldDecorator}
+                  onChange={this.onChangeEndTime}
+                />
+              </Col>
+            </Row>
+          </Form>
         </Modal>
       </div>
     );
   }
 
-  handleOk = () => {
-    const { onChange = () => {} } = this.props;
-    this.setState({
-      ModalText: 'The modal dialog will be closed after two seconds',
-      confirmLoading: true
-    });
-    setTimeout(
-      () => {
-        onChange({
-          popupScheduleVisible: false
-        });
+  handleOk = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      console.log('err of form: ', err);
+      console.log('Received values of form: ', values);
+      if (!err) {
+        const { onChange = () => {} } = this.props;
         this.setState({
-          confirmLoading: false
+          ModalText: 'The modal dialog will be closed after two seconds',
+          confirmLoading: true
         });
-      },
-      2000
-    );
+        setTimeout(
+          () => {
+            onChange({
+              popupScheduleVisible: false
+            });
+            this.setState({
+              confirmLoading: false
+            });
+          },
+          2000
+        );
+      } else {
+      }
+    });
   };
   handleCancel = () => {
     const { onChange = () => {} } = this.props;
-    console.log('Clicked cancel button:', onChange);
     onChange({
       popupScheduleVisible: false
     });
   };
   onChangeStartTime = startTime => {
-    this.setState({ startTime });
+    // this.setState({ startTime });
+    this.props.form.setFieldsValue({
+      startTime
+    });
   };
   onChangeEndTime = endTime => {
     this.setState({ endTime });
@@ -199,3 +209,5 @@ export default class Schedule extends PureComponent {
     });
   };
 }
+
+export default Form.create()(Schedule);
---
+++ components/TimePicker.js
@@ -0,0 +1,33 @@
+import { PureComponent } from 'react';
+import { Form, TimePicker } from 'antd';
+
+const format = 'HH:mm';
+
+const timePickerLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 10 }
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 14 }
+  }
+};
+
+const _TimePicker = ({ getFieldDecorator, onChange, label, field }) => (
+  <Form.Item {...timePickerLayout} label={label}>
+    {getFieldDecorator(field, {
+      rules: [
+        {
+          type: 'object',
+          required: true,
+          message: 'Required'
+        }
+      ],
+      onChange
+    })(<TimePicker format={format} style={{ width: '100%' }} />)}
+
+  </Form.Item>
+);
+
+export default _TimePicker;
